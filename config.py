b35d56d75c65b5c60f87d9730ff2066fa0416579
Tim Dunn <txmboslxce@gmail.com>
2025-10-10 20:47:14 -0400

    Update configuration for production deployment

--- config.py
+++ config.py
@@ -6,9 +6,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-please-change-in-production'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'fantasy_league.db')
+        'sqlite:///' + os.path.join('/opt/render/project/src', 'fantasy_league.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
+    
+    @staticmethod
+    def init_app(app):
+        pass
     
 class DevelopmentConfig(Config):
     DEBUG = True
